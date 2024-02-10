01034a2d070a410ebb30433b808d433598c84b77
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-02-10 12:23:03 +0530

    downgraded to lower version to bypass tool requirement

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ To set up the project, follow these steps:
 Install required Python packages using pip:
 
 ```bash
-pip install langchain_experimental langchain_openai pandas flask tabulate python-dotenv
+pip install langchain_experimental==0.0.49 langchain_openai pandas flask tabulate python-dotenv
 ```
 Create a .env file in the project root directory and set your OpenAI API key:
 
@@ -19,12 +19,11 @@ Set up a virtual environment named api-venv:
 ```bash
 python -m venv api-venv
 ```
-Disable certain lines in 
-```bash 
-api-venv\Lib\site-packages\langchain\agents\react\agent.py
-```
- for experimentation by commenting out lines 93 to 97.
+Activate venv:
 
+```bash
+api-venv\Scripts\Activate.ps1
+```
 
 ### Run the API using the following command:
 
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from langchain_openai import OpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from dotenv import load_dotenv
+from langchain.agents.agent_types import AgentType
 
 load_dotenv()
 
@@ -21,26 +22,17 @@ def get_answer():
     try:
         data = request.get_json()
         reqPrompt = data.get('prompt', '').lower()
-
-        finalPrompt = {
-            "input": reqPrompt + ". 1) ignoring case. 2) use includes and not equal for string type datas.",
-            "tool_names":["pandas"],
-            "tools":{
-                "pandas":{
-                    "df": df.to_dict()
-                }
-            }
-        }
         
         # Create agent with pandas dataframe
         agent = create_pandas_dataframe_agent(llm, df, verbose=True)
         
         # Invoke the agent with the provided prompt
-        answer = agent.invoke(finalPrompt)
+        answer = agent.invoke(reqPrompt + ". 1) ignoring case. 2) use includes and not equal for string type datas.")
         
         return jsonify({"answer": answer.get("output")})
     
     except Exception as e:
+        print(e)
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':

d3a5f5d7c313a84f35799863cbdec6290cf1da30
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-02-12 09:55:35 +0530

    temp set to 0.2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 filePath = "data_new.csv"
 
-llm = OpenAI(temperature=0.4, model="gpt-3.5-turbo-instruct-0914")
+llm = OpenAI(temperature=0.2, model="gpt-3.5-turbo-instruct-0914")
 
 df = pd.read_csv(filePath).fillna(0)
 df.rename(columns={'category':'ownership_type','registered_address':'address','upper':'turnover_range_upper_boundary','lower':'turnover_range_lower_boundary'}, inplace=True)
